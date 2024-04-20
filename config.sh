043de434e812c3ccece1a22173ee4b380cc7fc19
pikim <pikim@web.de>
2024-04-20 18:28:54 +0200

    Move custom config section to a separate file

---
+++ config.sh
@@ -0,0 +1,8 @@
+## any additional mount points to monitor here, space delimited like "/jffs /nfs"
+disks="/tmp/mnt/sda1"
+
+## MQTT connection settings
+port="1883"
+addr="your_hassos_address"
+username="your_ha_mqtt_username"
+password="your_ha_mqtt_password"
--- variables.sh
+++ variables.sh
@@ -1,11 +1,5 @@
-## any additional mount points to monitor here, space delimited like "/jffs /nfs"
-disks="/tmp/mnt/SiteCom_32MB"
-
-## MQTT connection settings
-port="1883"
-addr="your_hassos_address"
-username="your_ha_mqtt_username"
-password="your_ha_mqtt_password"
+## read custom configuration
+source config.sh
 
 ## MQTT topic settings
 prefix="FreshTomato"
