a836b316a6d38eeb3081c88af15e0fc797d7499c
pikim <pikim@web.de>
2024-04-20 18:37:23 +0200

    Add total disk size and skip invalid disks

--- checkDisk.sh
+++ checkDisk.sh
@@ -5,10 +5,15 @@ source variables.sh
 for i in $disks; do
     used=0
     free=0
+    total=`df | grep \ $i$ | awk '{print $2}'`
     used=`df | grep \ $i$ | awk '{print $3}'`
     free=`df | grep \ $i$ | awk '{print $4}'`
     part=`df | grep \ $i$ | awk -F"/" '{ print $NF }'`
 
+    ## skip invalid disk names
+    [ "$part" == "" ] && continue
+
     mqtt_publish "disk $part used" $used '"icon": "mdi:harddisk", "state_class": "measurement", "device_class": "data_size", "unit_of_meas": "B", '
     mqtt_publish "disk $part free" $free '"icon": "mdi:harddisk", "state_class": "measurement", "device_class": "data_size", "unit_of_meas": "B", '
+    mqtt_publish "disk $part total" $total '"icon": "mdi:harddisk", "state_class": "measurement", "device_class": "data_size", "unit_of_meas": "B", '
 done
