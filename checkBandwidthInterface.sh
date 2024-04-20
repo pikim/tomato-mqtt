d3e696f3960f4f18cb311bd574f743d787f7c61c
pikim <pikim@web.de>
2024-04-20 18:54:32 +0200

    Allow to ignore interfaces

--- checkBandwidthInterface.sh
+++ checkBandwidthInterface.sh
@@ -1,8 +1,13 @@
 #!/bin/sh
 
+ignore="dpsta ifb0 ifb1 ifb2 ifb3"
+
 source variables.sh
 
 for i in `\ls -A /sys/class/net/`; do
+    ## skip interfaces from ignore list
+    [[ "$ignore" =~ "$i" ]] && continue
+
     rx=0
     tx=0
     rx=`cat /sys/class/net/$i/statistics/rx_bytes`
