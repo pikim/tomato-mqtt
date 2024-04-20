0d9b98bd3c87dea31e54195c8b3f5abf67adc69f
pikim <pikim@web.de>
2024-04-20 14:12:10 +0200

    Remove obsolete script

--- checkCPUTemp.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-
-source /jffs/tomato-grafana/variables.sh
-
-cpuTemp=`cat /proc/dmu/temperature  | grep -o '[0-9]\+'`
-
-curl -XPOST 'http://'$ifserver':'$ifport'/write?db='$ifdb -u $ifuser:$ifpass --data-binary 'temp.cpu.degrees value='$cpuTemp
