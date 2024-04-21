2d472f03d594c35bbb4c1114383b004c4345e617
pikim <pikim@web.de>
2024-04-21 22:09:30 +0200

    Add last process ID

--- checkLoad.sh
+++ checkLoad.sh
@@ -8,6 +8,7 @@ load5=`echo "$load" | awk '{print $2}'`
 load15=`echo "$load" | awk '{print $3}'`
 proc_run=`echo "$load" | awk '{print $4}' | awk -F '/' '{print $1}'`
 proc_total=`echo "$load" | awk '{print $4}' | awk -F '/' '{print $2}'`
+last_pid=`echo "$load" | awk '{print $5}'`
 uptime=`cat /proc/uptime | awk '{print $1}'`
 
 mqtt_publish "load 1m" $load1 '"icon": "mdi:cpu-64-bit", "state_class": "measurement", "entity_category": "diagnostic", '
@@ -16,3 +17,4 @@ mqtt_publish "load 15m" $load15 '"icon": "mdi:cpu-64-bit", "state_class": "measu
 mqtt_publish "uptime" $uptime '"icon": "mdi:clock", "state_class": "measurement", "entity_category": "diagnostic", "unit_of_meas": "s", '
 #mqtt_publish "processes running" $proc_run '"icon": "mdi:numeric", "state_class": "measurement", "entity_category": "diagnostic", '
 #mqtt_publish "processes existing" $proc_total '"icon": "mdi:numeric", "state_class": "measurement", "entity_category": "diagnostic", '
+#mqtt_publish "last process ID" $last_pid '"icon": "mdi:numeric", "state_class": "measurement", "entity_category": "diagnostic", '
