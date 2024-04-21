56e6eaea847e6de737296596f7cb09088338f2ea
pikim <pikim@web.de>
2024-04-21 22:02:54 +0200

    Comment less interesting sensors

--- checkLoad.sh
+++ checkLoad.sh
@@ -14,5 +14,5 @@ mqtt_publish "load 1m" $load1 '"icon": "mdi:cpu-64-bit", "state_class": "measure
 mqtt_publish "load 5m" $load5 '"icon": "mdi:cpu-64-bit", "state_class": "measurement", "entity_category": "diagnostic", '
 mqtt_publish "load 15m" $load15 '"icon": "mdi:cpu-64-bit", "state_class": "measurement", "entity_category": "diagnostic", '
 mqtt_publish "uptime" $uptime '"icon": "mdi:clock", "state_class": "measurement", "entity_category": "diagnostic", "unit_of_meas": "s", '
-mqtt_publish "processes running" $proc_run '"icon": "mdi:numeric", "state_class": "measurement", "entity_category": "diagnostic", '
-mqtt_publish "processes existing" $proc_total '"icon": "mdi:numeric", "state_class": "measurement", "entity_category": "diagnostic", '
+#mqtt_publish "processes running" $proc_run '"icon": "mdi:numeric", "state_class": "measurement", "entity_category": "diagnostic", '
+#mqtt_publish "processes existing" $proc_total '"icon": "mdi:numeric", "state_class": "measurement", "entity_category": "diagnostic", '
