44a5bd4fc1b32fa2a2fef02799d2d80fa0e57806
pikim <pikim@web.de>
2024-04-28 11:23:00 +0200

    Prepare for speedtest URL

--- speedTest.sh
+++ speedTest.sh
@@ -10,6 +10,7 @@ jitter=$(echo "$result" | awk -F\" '{print $8}')
 loss=$(echo "$result" | awk -F\" '{print $10}')
 down=$(echo "$result" | awk -F\" '{print $12}')
 up=$(echo "$result" | awk -F\" '{print $14}')
+#url=$(echo "$result" | awk -F\" '{print $29}')
 
 # calculate kbps
 down=$((down/125))
@@ -18,3 +19,4 @@ up=$((up/125))
 mqtt_publish -e "speedtest ping" -s "$ping" -o '"icon": "mdi:timer-outline", "state_class": "measurement", "entity_category": "diagnostic", "unit_of_meas": "ms",'
 mqtt_publish -e "speedtest upload" -s "$up" -o '"icon": "mdi:speedometer", "state_class": "measurement", "entity_category": "diagnostic", "unit_of_meas": "kbit/s",'
 mqtt_publish -e "speedtest download" -s "$down" -o '"icon": "mdi:speedometer", "state_class": "measurement", "entity_category": "diagnostic", "unit_of_meas": "kbit/s",'
+#mqtt_publish -e "speedtest URL" -i "text" -s "$url" -o '"icon": "mdi:web", "entity_category": "diagnostic",'
