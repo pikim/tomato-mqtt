47f576c2c1d9d778f32850b2222800b5a019cb0f
pikim <pikim@web.de>
2024-04-30 21:49:37 +0200

    Run asynchronously

--- checkPing.sh
+++ checkPing.sh
@@ -2,12 +2,17 @@
 
 . "${SCRIPTPATH}variables.sh"
 
-for i in $hosts; do
-    ping_host="$i"
-    ping_result=$(ping -c 10 $ping_host | tail -2)
+ping_host(){
+    host_name="$1"
+    ping_result=$(ping -c 10 $host_name | tail -2)
     ping_loss=$(echo "$ping_result" | tr ',' '\n' | grep "packet loss" | grep -o '[0-9]\+')
     ping_time=$(echo "$ping_result" | grep "round-trip" | cut -d " " -f 4 | cut -d "/" -f 1)
 
-    mqtt_publish -e "ping ${ping_host//./_} loss" -s "$ping_loss" -o '"icon": "mdi:percent", "state_class": "measurement", "entity_category": "diagnostic", "unit_of_meas": "%",'
-    mqtt_publish -e "ping ${ping_host//./_} time" -s "$ping_time" -o '"icon": "mdi:timer-outline", "state_class": "measurement", "entity_category": "diagnostic", "unit_of_meas": "ms",'
+    host_name="${host_name//./_}"
+    mqtt_publish -e "ping $host_name loss" -s "$ping_loss" -o '"icon": "mdi:percent", "state_class": "measurement", "entity_category": "diagnostic", "unit_of_meas": "%",'
+    mqtt_publish -e "ping $host_name time" -s "$ping_time" -o '"icon": "mdi:timer-outline", "state_class": "measurement", "entity_category": "diagnostic", "unit_of_meas": "ms",'
+}
+
+for i in $hosts; do
+    ping_host "$i" &
 done
