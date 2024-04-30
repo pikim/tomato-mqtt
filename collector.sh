ac4c508b2510cf16ef6df338a6b9f4f5f51012e4
pikim <pikim@web.de>
2024-04-30 21:28:09 +0200

    Rename script

--- collector.sh
+++ collector.sh
@@ -7,7 +7,7 @@ sh "${SCRIPTPATH}checkBandwidthInterface.sh" &
 sh "${SCRIPTPATH}checkConnections.sh" &
 sh "${SCRIPTPATH}checkWireless.sh" &
 sh "${SCRIPTPATH}checkClients.sh" &
+sh "${SCRIPTPATH}checkPing.sh" &
 sh "${SCRIPTPATH}checkLoad.sh" &
 sh "${SCRIPTPATH}checkDisk.sh" &
 sh "${SCRIPTPATH}checkMem.sh" &
-sh "${SCRIPTPATH}pingGoogle.sh" &
