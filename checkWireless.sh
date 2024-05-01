b453d2792e6ce9ddda9abae917ab3ed0d7553925
pikim <pikim@web.de>
2024-05-01 22:08:23 +0200

    Add noise level

--- checkWireless.sh
+++ checkWireless.sh
@@ -4,6 +4,10 @@
 
 eth1Temp=$(($(wl -i eth1 phy_tempsense|awk '{print $1}')/2+20))
 eth2Temp=$(($(wl -i eth2 phy_tempsense|awk '{print $1}')/2+20))
+eth1Noise=$(wl -i eth1 noise)
+eth2Noise=$(wl -i eth2 noise)
 
 mqtt_publish -e "WiFi 2G4 temperature" -s "$eth1Temp" -o '"icon": "mdi:thermometer", "state_class": "measurement", "entity_category": "diagnostic", "device_class": "temperature", "unit_of_meas": "°C",'
 mqtt_publish -e "WiFi 5G temperature" -s "$eth2Temp" -o '"icon": "mdi:thermometer", "state_class": "measurement", "entity_category": "diagnostic", "device_class": "temperature", "unit_of_meas": "°C",'
+mqtt_publish -e "WiFi 2G4 noise" -s "$eth1Noise" -o '"icon": "mdi:wifi-alert", "state_class": "measurement", "entity_category": "diagnostic", "device_class": "signal_strength", "unit_of_meas": "dB",'
+mqtt_publish -e "WiFi 5G noise" -s "$eth2Noise" -o '"icon": "mdi:wifi-alert", "state_class": "measurement", "entity_category": "diagnostic", "device_class": "signal_strength", "unit_of_meas": "dB",'
