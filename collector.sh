8b2c617b927b5745f9276868434563161c56382f
pikim <pikim@web.de>
2024-05-08 07:36:52 +0200

    Re-order the scripts

--- collector.sh
+++ collector.sh
@@ -1,13 +1,13 @@
 #!/bin/sh
 
 sh "${SCRIPTPATH}checkCPU.sh"
-sh "${SCRIPTPATH}checkAdBlock.sh" &
-sh "${SCRIPTPATH}checkAccessRestriction.sh" &
-sh "${SCRIPTPATH}checkBandwidthInterface.sh" &
-sh "${SCRIPTPATH}checkConnections.sh" &
-sh "${SCRIPTPATH}checkWireless.sh" &
-sh "${SCRIPTPATH}checkClients.sh" &
-sh "${SCRIPTPATH}checkPing.sh" &
-sh "${SCRIPTPATH}checkLoad.sh" &
-sh "${SCRIPTPATH}checkDisk.sh" &
 sh "${SCRIPTPATH}checkMem.sh" &
+sh "${SCRIPTPATH}checkDisk.sh" &
+sh "${SCRIPTPATH}checkLoad.sh" &
+sh "${SCRIPTPATH}checkPing.sh" &
+sh "${SCRIPTPATH}checkClients.sh" &
+sh "${SCRIPTPATH}checkWireless.sh" &
+sh "${SCRIPTPATH}checkConnections.sh" &
+sh "${SCRIPTPATH}checkBandwidthInterface.sh" &
+sh "${SCRIPTPATH}checkAccessRestriction.sh" &
+sh "${SCRIPTPATH}checkAdBlock.sh" &
