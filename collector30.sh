a749c7ca28883cf995ea40635b0955ea9bb3c888
pikim <pikim@web.de>
2024-05-08 07:38:00 +0200

    Don't run all scripts twice a minute

--- collector30.sh
+++ collector30.sh
@@ -1,3 +1,7 @@
 #!/bin/sh
 sleep 30
-sh "${SCRIPTPATH}collector.sh"
+#sh "${SCRIPTPATH}collector.sh"
+
+# only the scripts below need to run twice a minute
+sh "${SCRIPTPATH}checkAccessRestriction.sh" &
+sh "${SCRIPTPATH}checkAdBlock.sh" &
