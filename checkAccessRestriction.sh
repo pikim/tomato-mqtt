f53fdfec13c036f5e88ac9d94ec139ccfbaa55ec
pikim <pikim@web.de>
2024-05-31 13:48:10 +0200

    Improve switch entity handling

--- checkAccessRestriction.sh
+++ checkAccessRestriction.sh
@@ -54,23 +54,20 @@ while IFS= read -r rrule; do
     fi
 #    echo "$name \"$friendly\""
 
-    ## create topic if it doesn't exist and skip the following steps
-    if [ "$meta" = "" ]; then
-        echo "Creating entity $name"
-        mqtt_publish -e "$name" -f "$friendly" -i "$integration" -s "$enable_old" -o "\"command_topic\": \"homeassistant/${integration}/${name// /_}/state\", \"payload_off\": \"0\", \"payload_on\": \"1\", \"icon\": \"mdi:network-off-outline\","
-        continue
-    fi
-
+    enable_new=null
     ## convert state
     case $state in
         "1") enable_new="1" ;;
         "0") enable_new="0" ;;
         "on") enable_new="1" ;;
         "off") enable_new="0" ;;
-        *) echo "Unknown parameter passed: \"$state\". Setting it to \"1\" instead."
-            enable_new="1" ;;
+        *) echo "Unknown parameter passed: \"$state\". Using current state: \"$enable_old\"."
+            enable_new="$enable_old" ;;
     esac
 #    echo "$enable_old $enable_new"
+
+    ## publish entity
+    mqtt_publish -e "$name" -f "$friendly" -i "$integration" -s "$enable_new" -o "\"command_topic\": \"homeassistant/${integration}/${name// /_}/state\", \"payload_off\": \"0\", \"payload_on\": \"1\", \"icon\": \"mdi:network-off-outline\","
 
     ## skip following steps if nothing has changed
     if [ "$enable_old" = "$enable_new" ]; then
--- checkAdBlock.sh
+++ checkAdBlock.sh
@@ -22,22 +22,20 @@ fi
 meta=$(jq ". | select(.friendly_name == \"$name\")" "$entity_file")
 state=$(echo "$meta" | jq -r '.state')
 
-## create topic if it doesn't exist
-if [ "$meta" = "" ]; then
-    echo "Creating entity $name"
-    mqtt_publish -e "$name" -i "$integration" -s "$enable_old" -o "\"command_topic\": \"homeassistant/${integration}/${name// /_}/state\", \"payload_off\": \"0\", \"payload_on\": \"1\", \"icon\": \"mdi:advertisements-off\","
-fi
-
+enable_new=null
 ## convert state
 case $state in
     "1") enable_new="1" ;;
     "0") enable_new="0" ;;
     "on") enable_new="1" ;;
     "off") enable_new="0" ;;
-    *) echo "Unknown parameter passed: \"$state\". Setting it to \"1\" instead."
-        enable_new="1" ;;
+    *) echo "Unknown parameter passed: \"$state\". Using current state: \"$enable_old\"."
+        enable_new="$enable_old" ;;
 esac
 echo "AdBlock states: router=${enable_old}; hass=${enable_new}"
+
+## publish entity
+mqtt_publish -e "$name" -i "$integration" -s "$enable_new" -o "\"command_topic\": \"homeassistant/${integration}/${name// /_}/state\", \"payload_off\": \"0\", \"payload_on\": \"1\", \"icon\": \"mdi:advertisements-off\","
 
 ## leave if nothing has changed
 if [ "$enable_old" = "$enable_new" ]; then
